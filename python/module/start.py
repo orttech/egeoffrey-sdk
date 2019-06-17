d887273df45c770e35ab01ac988e6e0c2db7eb26
user2684 <user2684@users.noreply.github.com>
2019-06-17 19:16:00 +0200

    Updated watchdog and notification

--- python/module/start.py
+++ python/module/start.py
@@ -3,7 +3,7 @@ import time
 from sdk.python.module.watchdog import Watchdog
 
 # run the watchdog module which will load and start all the modules listed in MYHOUSE_MODULES
-watchdog = Watchdog()
+watchdog = Watchdog("system", "watchdog")
 watchdog.daemon = True
 watchdog.start()
 
