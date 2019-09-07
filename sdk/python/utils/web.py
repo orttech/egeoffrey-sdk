b40ac2c883f8cf42aba68b31be2b66e8b482f850
user2684 <user2684@users.noreply.github.com>
2019-09-07 11:03:51 +0200

    configuration messages are queued while the module is yet not configured

--- sdk/python/utils/web.py
+++ sdk/python/utils/web.py
@@ -4,11 +4,14 @@
 # Python: 
 
 import requests
+from urllib3.exceptions import InsecureRequestWarning
+# Suppress only the single warning from urllib3 needed
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 # request a given url
 def get(url,username=None,password=None,binary=False,params={},timeout=30):
-	if username is not None: request = requests.get(url,params=params,auth=(username,password),timeout=timeout,verify=False)
-	else: request = requests.get(url,params=params,timeout=30,verify=False)
+	if username is not None: request = requests.get(url, params=params, auth=(username,password), timeout=timeout, verify=False)
+	else: request = requests.get(url, params=params, timeout=30, verify=False)
 	if binary: return request.content
 	else: return request.text
 
